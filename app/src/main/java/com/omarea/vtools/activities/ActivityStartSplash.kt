bfe1b8e2b25c7d41c66589fa1c83c81abf9c39d4
JiaMing746 <3961215806@qq.com>
2024-06-09 09:15:51 +0800

    Add files via upload

--- app/src/main/java/com/omarea/vtools/activities/ActivityStartSplash.kt
+++ app/src/main/java/com/omarea/vtools/activities/ActivityStartSplash.kt
@@ -4,18 +4,17 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.core.app.ActivityCompat
-import androidx.core.content.PermissionChecker
 import android.util.TypedValue
 import android.view.View
-import android.widget.TextView
-import com.omarea.common.shell.ShellExecutor
-import com.omarea.krscript.executor.ScriptEnvironmen
+import android.widget.Button
+import android.widget.CompoundButton
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+import com.omarea.Scene
 import com.omarea.common.ui.DialogHelper
 import com.omarea.common.ui.ThemeMode
 import com.omarea.library.permissions.GeneralPermissions
@@ -23,49 +22,97 @@ import com.omarea.permissions.Busybox
 import com.omarea.permissions.CheckRootStatus
 import com.omarea.permissions.WriteSettings
 import com.omarea.store.SpfConfig
-import com.omarea.kr.KrScriptConfig
-import kotlinx.android.synthetic.main.activity_splash.*
-import java.io.BufferedReader
-import java.io.DataOutputStream
 import com.omarea.vtools.R
+import kotlinx.android.synthetic.main.activity_start_splash.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
 
 class ActivityStartSplash : Activity() {
+    companion object {
+        var finished = false
+    }
+
+    private lateinit var globalSPF: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        globalSPF = getSharedPreferences(SpfConfig.GLOBAL_SPF, Context.MODE_PRIVATE)
+
+        val themeMode = ThemeSwitch.switchTheme(this)
         super.onCreate(savedInstanceState)
 
-        if (ScriptEnvironmen.isInited()) {
-            if (isTaskRoot) {
-                gotoHome()
-            }
-            return
-        }
-
-        setContentView(R.layout.activity_splash)
-        updateThemeStyle()
+        setContentView(R.layout.activity_start_splash)
+        updateThemeStyle(themeMode)
 
         checkPermissions()
     }
 
     /**
+     * 协议 同意与否
+     */
+    private fun initContractAction() {
+        val view = layoutInflater.inflate(R.layout.dialog_danger_agreement, null)
+        val dialog = DialogHelper.customDialog(this, view, false)
+        val btnConfirm = view.findViewById<Button>(R.id.btn_confirm)
+        val agreement = view.findViewById<CompoundButton>(R.id.agreement)
+        val timer = Timer()
+        var timeout = 30
+        var clickItems = 0
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                Scene.post {
+                    if (timeout > 0) {
+                        timeout --
+                        btnConfirm.text = timeout.toString() + "s"
+                    } else {
+                        timer.cancel()
+                        btnConfirm.text = "同意继续"
+                    }
+                }
+            }
+        }, 0, 1000)
+        view.findViewById<View>(R.id.btn_cancel).setOnClickListener {
+            timer.cancel()
+            dialog.dismiss()
+            finish()
+        }
+        btnConfirm.setOnClickListener {
+            if (!agreement.isChecked) {
+                return@setOnClickListener
+            }
+            if (timeout > 0 && clickItems < 10) { // 连点10次允许跳过倒计时
+                clickItems++
+                return@setOnClickListener
+            }
+
+            timer.cancel()
+            dialog.dismiss()
+            globalSPF.edit().putBoolean(SpfConfig.GLOBAL_SPF_CONTRACT, true).apply()
+            checkPermissions()
+        }
+    }
+
+    /**
      * 界面主题样式调整
      */
-    private fun updateThemeStyle() {
-        getWindow().setNavigationBarColor(getColorAccent())
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.setNavigationBarColor(getColor(R.color.splash_bg_color))
+    private fun updateThemeStyle(themeMode: ThemeMode) {
+        if (themeMode.isDarkMode) {
+            splash_root.setBackgroundColor(Color.argb(255, 0, 0, 0))
+            getWindow().setNavigationBarColor(Color.argb(255, 0, 0, 0))
         } else {
-            window.setNavigationBarColor(resources.getColor(R.color.splash_bg_color))
+            // getWindow().setNavigationBarColor(getColorAccent())
+            splash_root.setBackgroundColor(Color.argb(255, 255, 255, 255))
+            getWindow().setNavigationBarColor(Color.argb(255, 255, 255, 255))
         }
 
         //  得到当前界面的装饰视图
-        if (Build.VERSION.SDK_INT >= 21) {
-            val decorView = getWindow().getDecorView();
-            //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
-            val option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            decorView.setSystemUiVisibility(option);
-            //设置状态栏颜色为透明
-            getWindow().setStatusBarColor(Color.TRANSPARENT)
-        }
+        val decorView = window.decorView;
+        //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
+        val option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        decorView.systemUiVisibility = option
+        //设置状态栏颜色为透明
+        getWindow().setStatusBarColor(Color.TRANSPARENT)
     }
 
     private fun getColorAccent(): Int {
@@ -78,18 +125,26 @@ class ActivityStartSplash : Activity() {
      * 开始检查必需权限
      */
     private fun checkPermissions() {
-        start_logo.visibility = View.VISIBLE
-        checkRoot(Runnable {
-            start_state_text.text = getString(R.string.pio_permission_checking)
-            hasRoot = true
+        checkRoot()
+    }
 
-            /*
-            checkFileWrite(Runnable {
-                startToFinish()
-            })
-            */
-            startToFinish()
-        })
+    private class CheckFileWrite(private val context: ActivityStartSplash) : Runnable {
+        override fun run() {
+            context.start_state_text.text = "检查并获取必需权限……"
+            context.hasRoot = true
+
+            context.checkFileWrite(InstallBusybox(context))
+        }
+    }
+
+    private class InstallBusybox(private val context: ActivityStartSplash) : Runnable {
+        override fun run() {
+            context.start_state_text.text = "检查Busybox是否安装..."
+            Busybox(context).forceInstall {
+                context.startToFinish()
+            }
+        }
+
     }
 
     private fun checkPermission(permission: String): Boolean = PermissionChecker.checkSelfPermission(this.applicationContext, permission) == PermissionChecker.PERMISSION_GRANTED
@@ -98,12 +153,16 @@ class ActivityStartSplash : Activity() {
      * 检查权限 主要是文件读写权限
      */
     private fun checkFileWrite(next: Runnable) {
-        Thread(Runnable {
-            CheckRootStatus.grantPermission(this)
+        val activity = this
+        GlobalScope.launch(Dispatchers.Main) {
+            if (hasRoot) {
+                GeneralPermissions(activity).grantPermissions()
+            }
+
             if (!(checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE) && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE))) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     ActivityCompat.requestPermissions(
-                            this@ActivityStartSplash,
+                            activity,
                             arrayOf(
                                     Manifest.permission.READ_EXTERNAL_STORAGE,
                                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -115,7 +174,7 @@ class ActivityStartSplash : Activity() {
                     )
                 } else {
                     ActivityCompat.requestPermissions(
-                            this@ActivityStartSplash,
+                            activity,
                             arrayOf(
                                     Manifest.permission.READ_EXTERNAL_STORAGE,
                                     Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -126,22 +185,37 @@ class ActivityStartSplash : Activity() {
                     )
                 }
             }
-            myHandler.post {
-                next.run()
+
+            // 请求写入设置权限
+            val writeSettings = WriteSettings()
+            if (!writeSettings.checkPermission(applicationContext)) {
+                if (hasRoot) {
+                    writeSettings.setPermissionByRoot(applicationContext)
+                } else {
+                    writeSettings.requestPermission(applicationContext)
+                }
             }
-        }).start()
+            next.run()
+        }
     }
 
     private var hasRoot = false
-    private var myHandler = Handler()
 
-    private fun checkRoot(next: Runnable) {
-        CheckRootStatus(this, next).forceGetRoot()
+    private fun checkRoot() {
+        val disableSeLinux = globalSPF.getBoolean(SpfConfig.GLOBAL_SPF_DISABLE_ENFORCE, false)
+        CheckRootStatus(this, {
+            if (globalSPF.getBoolean(SpfConfig.GLOBAL_SPF_CONTRACT, false)) {
+                CheckFileWrite(this).run()
+            } else {
+                initContractAction()
+            }
+        }, disableSeLinux, InstallBusybox(this)).forceGetRoot()
     }
 
     /**
      * 启动完成
      */
+    
     private fun startToFinish() {
         start_state_text.text = getString(R.string.pop_started)
 
@@ -156,14 +230,11 @@ class ActivityStartSplash : Activity() {
     }
 
     private fun gotoHome() {
-        if (this.intent != null && this.intent.hasExtra("JumpActionPage") && this.intent.getBooleanExtra("JumpActionPage", false)) {
-            val actionPage = Intent(this.applicationContext, ActionPage::class.java)
-            actionPage.putExtras(this.intent)
-            startActivity(actionPage)
-        } else {
-            val home = Intent(this.applicationContext, ActivityStartSplash::class.java)
-            startActivity(home)
-        }
+      start_state_text.text = "Completed!"
+
+        val intent = Intent(this.applicationContext, ActivityMain::class.java)
+        startActivity(intent)
+        finished = true
         finish()
     }
 
