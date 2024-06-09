cfed033dae590689c3e87b80b79aac88e9c80c3d
JiaMing746 <3961215806@qq.com>
2024-06-09 12:50:28 +0800

    Add files via upload

--- app/src/main/java/com/omarea/vtools/fragments/FragmentDonate.kt
+++ app/src/main/java/com/omarea/vtools/fragments/FragmentDonate.kt
@@ -1,22 +1,16 @@
 package com.omarea.vtools.fragments
 
-import android.content.ComponentName
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridLayout
-import android.widget.LinearLayout
-import android.widget.Toast
-import com.omarea.permissions.CheckRootStatus
-import com.omarea.utils.AlipayDonate
+import androidx.fragment.app.Fragment
 import com.omarea.vtools.R
 import kotlinx.android.synthetic.main.fragment_donate.*
 
-
-class FragmentDonate : androidx.fragment.app.Fragment(), View.OnClickListener {
+class FragmentDonate : Fragment(), View.OnClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? =
@@ -31,69 +25,20 @@ class FragmentDonate : androidx.fragment.app.Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+        pay_alipay.setOnClickListener {
+            val sendIntent = Intent()
+            sendIntent.action = Intent.ACTION_SEND
+            sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link))
+            sendIntent.type = "text/plain"
+            startActivity(sendIntent)
+        }
 
-        bindClickEvent(nav_share)
-        bindClickEvent(nav_qq)
-    }
-
-    private fun bindClickEvent(view: View) {
-        view.setOnClickListener(this)
-        if (!CheckRootStatus.lastCheckResult && "root".equals(view.getTag())) {
-            view.isEnabled = false
+        pay_wxpay.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=DnuSkhJ_x1lHKBxfi9GtbUijy43awAPW&authKey=RUtBy2exLLjSAM0ceIQQQP5yDusp91dAY%2BxTJDxxgvClkXBFDuFrL7XgzrFijIZi&noverify=0&group_code=813807778")))
         }
     }
 
     override fun onClick(v: View?) {
-        v?.run {
-            if (!CheckRootStatus.lastCheckResult && "root".equals(getTag())) {
-                Toast.makeText(context, "没有获得ROOT权限，不能使用本功能", Toast.LENGTH_SHORT).show()
-                return
-               }
-        }
-    }
-
-    private fun tryOpenApp(packageName: String) {
-        val pm = context!!.packageManager
-        if (packageName.equals("com.omarea.gesture")) {
-            try {
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                intent.setComponent(ComponentName("com.omarea.gesture", "com.omarea.gesture.SettingsActivity"))
-                startActivity(intent)
-                return
-            } catch (ex: java.lang.Exception) {
-            }
-        } else if (packageName.equals("com.omarea.filter")) {
-            try {
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                intent.setComponent(ComponentName("com.omarea.filter", "com.omarea.filter.SettingsActivity"))
-                startActivity(intent)
-                return
-            } catch (ex: java.lang.Exception) {
-            }
-        }
-
-        try {
-            val intent = pm.getLaunchIntentForPackage(packageName)
-            if (intent != null) {
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-                return
-            }
-        } catch (ex: java.lang.Exception) {
-        }
-
-        openUrl("http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=DnuSkhJ_x1lHKBxfi9GtbUijy43awAPW&authKey=RUtBy2exLLjSAM0ceIQQQP5yDusp91dAY%2BxTJDxxgvClkXBFDuFrL7XgzrFijIZi&noverify=0&group_code=813807778" )
-    }
-
-    private fun openUrl(link: String) {
-        try {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-        } catch (ex: Exception) {
-        }
+        // Handle other click events if needed
     }
 }
